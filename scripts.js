a5cc4931174ca1210caf5574d9bc235de2b7e37c
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2025-01-28 16:29:24 -0500

    Add comments for readability

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ function gameBoard() {
     const columns = 3;
     const board = [];
 
+    // Create 2D Array of game board.
     for (let r = 0; r < rows; r++) {
         board[r] = [];
         for (let c = 0; c < columns; c++) {
@@ -10,8 +11,10 @@ function gameBoard() {
         }
     }
 
+
     const getBoard = () => board;
 
+    // Place symbol in cell if empty, otherwise return false.
     function placeSymbol(row, column, player) {
         const cellChoice = board[row][column]
 
@@ -23,6 +26,7 @@ function gameBoard() {
         }
     };
 
+    // Display board in console with cell values.
     function displayBoard() {
         const printBoard = board.map((row) => row.map((cell) => cell.getValue()));
         console.log(printBoard);
@@ -31,24 +35,31 @@ function gameBoard() {
     return { getBoard, placeSymbol, displayBoard };
 };
 
+// Get / Set cell values.
 function Cell() {
+    // Default value of empty.
     let value = "";
 
+    // Change cell value to player symbol.
     const setValue = (player) => {
         value = player
     };
 
+    // Grab current cell value.
     const getValue = () => value;
 
     return { setValue, getValue };
 };
 
+// Controller of game...
 function gameController(
+    // Set default player names (can be changed in UI).
     playerOneName = "Player 1",
     playerTwoName = "Player 2"
 ) {
     const board = gameBoard();
 
+    // Define players and their symbols.
     const players = [
         {
             name: playerOneName,
@@ -60,8 +71,10 @@ function gameController(
         }
     ];
 
+    // Set active player.
     let activePlayer = players[0];
 
+    // Switch active player based on turn.
     const switchPlayerTurn = () => {
         if (activePlayer === players[0]) {
             activePlayer = players[1];
@@ -70,30 +83,38 @@ function gameController(
         };
     };
 
+    // Get the active player...
     const getActivePlayer = () => activePlayer;
 
+    // Display board for new round.
     const printNewRound = () => {
         board.displayBoard();
         console.log(`${getActivePlayer().name}'s turn`);
     }
 
+    // Play round based on chosen row & column.
     const playRound = (row, column) => {
+        // If cell choice is not empty, replay turn.
         if (!board.placeSymbol(row, column, getActivePlayer().symbol)) {
             console.log("You cannot place your symbol here!")
             !switchPlayerTurn();
         } else {
+            // Play turn if empty cell.
             console.log(`Placing ${getActivePlayer().name}'s ${getActivePlayer().symbol} in row ${row}, column ${column}`);
             board.placeSymbol(row, column, getActivePlayer().symbol);
         }
 
+        // Check for winner (3 consecutive cells).
         const checkWinner = () => {
-            
+
         }
 
+        // Switch player turn and print new round if turn successful.
         switchPlayerTurn();
         printNewRound();
     };
 
+    // Print new round if player cannot place on cell.
     printNewRound();
 
     return { playRound, getActivePlayer };
